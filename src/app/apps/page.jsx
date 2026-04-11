69cd885e0380623390944f3fb9bf1a9de48f20d6
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-04-11 16:10:20 +0600

    Add daisyUI integration and create new pages for Apps, Installation, and Not Found

---
+++ src/app/apps/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppsPage = () => {
+    return (
+        <div>
+            <h2>
+                Welcome to the Apps Page
+            </h2>
+        </div>
+    );
+};
+
+export default AppsPage;
